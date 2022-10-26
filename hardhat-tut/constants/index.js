5759427ea05720ec545cac4d49d3966f59cf75bd
Chirag <56735482+Chirag018@users.noreply.github.com>
2022-10-26 17:02:52 +0530

    backend comit

---
+++ hardhat-tut/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x7c3Da1675362Cfb05c2E0242e6474Cc806b77753";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
---
+++ hardhat-tut/contracts/CryptoDevToken.sol
@@ -0,0 +1,62 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./ICryptoDevs.sol";
+
+contract CryptoDevToken is ERC20, Ownable {
+    uint256 public constant tokenPrice = 0.001 ether;
+    uint256 public constant tokensPerNFT = 10 * 10**18;
+    uint256 public constant maxTotalSupply = 10000 * 10**18;
+    ICryptoDevs CryptoDevsNFT;
+
+    mapping(uint256 => bool) public tokenIdsClaimed;
+
+    constructor(address _cryptoDevsContract) ERC20("Crypto Dev Token", "CD") {
+        CryptoDevsNFT = ICryptoDevs(_cryptoDevsContract);
+    }
+
+    function mint(uint256 amount) public payable {
+        uint256 _requiredAmount = tokenPrice * amount;
+        require(msg.value >= _requiredAmount, "ether sent is incorrect");
+        uint256 amountWithDecimals = amount * 10**18;
+        require(
+            (totalSupply() + amountWithDecimals) <= maxTotalSupply,
+            "exceeds the max supply available."
+        );
+
+        _mint(msg.sender, amountWithDecimals);
+    }
+
+    function claim() public {
+        address sender = msg.sender;
+        uint256 balance = CryptoDevsNFT.balanceOf(sender);
+        require(balance > 0, "u dont own any crypto dev  nft");
+        uint256 amount = 0;
+
+        for (uint256 i = 0; i < balance; i++) {
+            uint256 tokenId = CryptoDevsNFT.tokenOfOwnerByIndex(sender, i);
+            if (!tokenIdsClaimed[tokenId]) {
+                amount += 1;
+                tokenIdsClaimed[tokenId] = true;
+            }
+        }
+        require(amount > 0, "u have already claimed all the tokens");
+
+        _mint(msg.sender, amount * tokensPerNFT);
+    }
+
+    function withdraw() public onlyOwner {
+        address _owner = owner();
+        uint256 amount = address(this).balance;
+        (bool sent, ) = _owner.call{value: amount}("");
+        require(sent, "Failed to send ether");
+    }
+
+    receive() external payable {}
+
+    fallback() external payable {}
+}
+
+//crypto devs token contract address 0xf8607E25D2FE943deA2fbFde33B7E53EDa205b5f
